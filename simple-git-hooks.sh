42cc0f4c3fe38e0b77b33f310e099ae97e5bc79e
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2022-03-20 00:37:20 +0800

    fix: fix simple-git-hooks does not read custom config file correctly
    Signed-off-by: Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>

--- simple-git-hooks.sh
+++ simple-git-hooks.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
 DIRNAME=$(dirname "$0")
+CONFIG="$DIRNAME/.simple-git-hooks.json"
 
-./node_modules/.bin/simple-git-hooks "$DIRNAME"/.simple-git-hooks.json
+# NOTE(motss): This is a workaround for the fact that the `simple-git-hooks` does not support
+# custom configuration file.
+#
+# 1. To temporarily copy the configuration file to `pwd`
+cp "$CONFIG" ./.simple-git-hooks.json
+
+# 2. Run the `simple-git-hooks`
+./node_modules/.bin/simple-git-hooks
+
+# 3. Remove the configuration file from `pwd`
+rm -rf ./.simple-git-hooks.json
