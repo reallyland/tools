cad640c87cf0c53434873057f100c0c012e921aa
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2022-03-19 20:30:26 +0800

    feat: add publish.yml generator
    Signed-off-by: Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>

---
+++ init-github-publish-yml.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+DIRNAME="$(dirname "$0")"
+CONTENT="$(cat "$DIRNAME/.github/workflows/publish.yml")"
+DEST_DIR="./.github2/workflows"
+
+if [ -z "$DEST" ]; then
+  printf "[INFO] %s\n" "Creating $DEST_DIR..."
+
+  mkdir -p "$DEST_DIR"
+fi
+
+DEST_FILE="$DEST_DIR/publish.yml"
+
+printf "[INFO] %s\n" "Creating $DEST_FILE..."
+
+printf "%s" "$CONTENT" > "$DEST_FILE"
+
+printf "[INFO] %s\n" "Done."
