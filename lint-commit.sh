00578b5b7bed538bb4fffa94970de735fb883dc4
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2022-03-19 23:37:44 +0800

    refactor: use .commitlintrc.json
    Signed-off-by: Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>

--- lint-commit.sh
+++ lint-commit.sh
@@ -1,3 +1,26 @@
 #!/bin/sh
 
-npm x -y -- commitlint@latest --edit
+DIRNAME=$(dirname "$0")
+CONFIG="$DIRNAME/.commitlintrc.json"
+DEBUG=false
+
+while getopts "c:d:" flag
+do
+  case "$flag" in
+    c) CONFIG=${OPTARG} ;;
+    d) DEBUG=true ;;
+    *) printf "%s" "
+usage: $0
+
+[-c] Path to ESLint config file
+[-d] Print debug messages
+" >&2
+       exit 1 ;;
+  esac
+done
+
+if [ "$DEBUG" = true ]; then
+  printf "[DEBUG] CONFIG: %s\n" "$CONFIG"
+fi
+
+npm x -y -- commitlint@latest --config "$CONFIG" --edit
