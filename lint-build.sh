b7fee77a9dccb7d495481b5fae1feffa38b1acda
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2022-07-10 18:27:41 +0800

    feat: add -s or --src in lint-build.sh
    Signed-off-by: Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>

--- lint-build.sh
+++ lint-build.sh
@@ -6,29 +6,34 @@ DEBUG=false
 FIX=false
 NPM_BIN=$(npm bin);
 
-while getopts "c:d:f:" flag
+while getopts "c:d:f:s:" flag
 do
   case "$flag" in
     c) CONFIG=${OPTARG} ;;
     d) DEBUG=true ;;
     f) FIX=true ;;
+    s) SRC=${OPTARG} ;;
     *) printf "%s" "
 usage: $0
 
 [-c] Path to ESLint config file
 [-d] Print debug messages
 [-f] Apply ESLint fixes
+[-s] Path to source files
 " >&2
        exit 1 ;;
   esac
 done
 
 if [ "$DEBUG" = true ]; then
-  printf "[DEBUG] CONFIG: %s\n[DEBUG] FIX: %s\n" "$CONFIG" "$FIX"
-fi
+  "$NPM_BIN"/eslint "${SRC:-src/**/*.ts}" --ext .js,.jsx,.ts,.tsx --config "$CONFIG" --debug --fix-dry-run
 
-if [ "$FIX" = true ]; then
-  "$NPM_BIN"/eslint src/**/*.ts --ext .js,.jsx,.ts,.tsx --config "$CONFIG" --fix
+  printf "\n"
+  printf "[DEBUG] CONFIG: %s\n" "$CONFIG"
+  printf "[DEBUG] FIX: %s\n" "$FIX"
+  printf "[DEBUG] SRC: %s\n" "$SRC"
+elif [ "$FIX" = true ]; then
+  "$NPM_BIN"/eslint "${SRC:-src/**/*.ts}" --ext .js,.jsx,.ts,.tsx --config "$CONFIG" --fix
 else
-  "$NPM_BIN"/eslint src/**/*.ts --ext .js,.jsx,.ts,.tsx --config "$CONFIG"
+  "$NPM_BIN"/eslint "${SRC:-src/**/*.ts}" --ext .js,.jsx,.ts,.tsx --config "$CONFIG"
 fi
