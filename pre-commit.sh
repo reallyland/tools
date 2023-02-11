82b921694eae0a25855b78d95fc31bab19ee7e62
The web walker <wes.ngrongsen@gmail.com>
2023-02-12 00:16:31 +0800

    fix: deprecate npm bin (#4)
    Signed-off-by: Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-NPM_BIN=$(npm bin)
 NPM_ROOT=$(npm root)
 
-"$NPM_BIN"/package-check && \
-"$NPM_BIN"/nano-staged --config "$NPM_ROOT"/@reallyland/tools/.nano-staged.json && \
-"$NPM_BIN"/tsc --noEmit
+npm x -- package-check && \
+npm x -- nano-staged --config "$NPM_ROOT"/@reallyland/tools/.nano-staged.json && \
+npm x -- tsc --noEmit
