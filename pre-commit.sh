6f6403200119ba4cbe95992cd09deb907581a24e
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2023-04-15 17:15:47 +0800

    fix: fix pre-commit
    Signed-off-by: Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>

--- pre-commit.sh
+++ pre-commit.sh
@@ -2,6 +2,6 @@
 
 NPM_ROOT=$(npm root)
 
-npm x -- package-check && \
-npm x -- nano-staged --config "$NPM_ROOT"/@reallyland/tools/.nano-staged.json && \
+npm x -y --package=@skypack/package-check@latest -- package-check && \
+npm x -y --package=nano-staged@latest -- nano-staged --config "$NPM_ROOT"/@reallyland/tools/.nano-staged.json && \
 npm x -- tsc --noEmit
