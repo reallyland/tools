9e4e41dcfa47d78975df754fb8c70cb50c3610ba
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2023-09-19 17:57:15 +0800

    fix: include nano-staged in husky setup script
    Signed-off-by: Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>

--- init-husky.sh
+++ init-husky.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-printf "[INFO] Setting up husky...\n"
+DIRNAME=$(dirname "$0")
+CONFIG="$DIRNAME/.base.nano-staged.json"
+
+printf "[INFO] Setting up husky & nano-staged...\n"
+
+printf "[INFO] Running husky-init...\n"
 pnpm dlx husky-init && pnpm install
+
+printf "[INFO] Adding nano-staged.json...\n"
+cp "$CONFIG" ./nano-staged.json
 
 printf "[INFO] Adding scripts.{commit-msg,pre-commit,prepare} to root workspace...\n"
 npm pkg set scripts.commit-msg="sh $(npm root)/@reallyland/tools/lint-commit.sh"
@@ -11,4 +19,4 @@ npm pkg set scripts.prepare="sh $(npm root)/@reallyland/tools/prepare.sh"
 printf "[INFO] Installing husky...\n"
 pnpm prepare
 
-printf "[INFO] husky is ready!\n"
+printf "[INFO] husky & nano-staged is ready!\n"
